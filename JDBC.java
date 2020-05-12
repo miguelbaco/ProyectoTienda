f5d2b0d32b7f074e0d74fa7a6c0d63e481fc93bc
miguelbaco <m.a.bacomorillo@gmail.com>
2020-05-12 17:45:54 +0200

    modificado paulino

--- JDBC.java
+++ JDBC.java
@@ -1,20 +1,30 @@
 import java.sql.*;
-import javax.annotation.Resource;
 import javax.sql.DataSource;
+import org.sqlite.SQLiteDataSource;
 //esta clase es para crear la tabla. para poder hacerlo, primero hay que poseer un archivo con extension .db
-
 public class JDBC {
-	public static void main(String[] args) {
-		Connection conn = null;
-		private DataSource dataSource;
-		try{
-			String sql = "CREATE TABLE IF NOT EXISTS compra (Cliente TEXT,Producto TEXT,Cantidad Double,Precio DOUBLE,ID INTEGER, Fecha TIMESTAMP);";
-			conn = dataSource.getConnection();
-			Statement statement = conn.createStatement();
-			statement.executeUpdate(sql);
-		}
-		catch (SQLException e) {
+        private SQLiteDataSource dataSource;  // sería interesante inyectar mediante setter
+        public void setDataSource(SQLiteDataSource d) {
+                dataSource = d;
+        }
+        public SQLiteDataSource getDataSource() {
+                return dataSource;
+        }
+        public JDBC() {
+                dataSource = new SQLiteDataSource();
+        }
+        public static void main(String[] args) {
+                JDBC obj = new JDBC();
+                (obj.getDataSource()).setUrl("jdbc:sqlite:compra.db");
+                Connection conn = null;
+                try{
+                        String sql = "CREATE TABLE IF NOT EXISTS compra (Cliente TEXT,Producto TEXT,Cantidad Double,Precio DOUBLE,ID INTEGER, Fecha TIMESTAMP);";
+                        conn = obj.getDataSource().getConnection();
+                        Statement statement = conn.createStatement();
+                        statement.executeUpdate(sql);
+                }
+                catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-	}
+        }
 }
